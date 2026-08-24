6530822ed76a380a6ef4c491cac17db6e4d63b56
Gloria <cchira@outlook.com>
2026-08-23 22:18:33 -0500

    Build TechHealth VPC network with AWS CDK

--- bin/techhealth-modernization.ts
+++ bin/techhealth-modernization.ts
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
+
+import * as cdk from 'aws-cdk-lib';
 import { TechhealthModernizationStack } from '../lib/techhealth-modernization-stack';
 
 const app = new cdk.App();
-new TechhealthModernizationStack(app, 'TechhealthModernizationStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new TechhealthModernizationStack(
+  app,
+  'TechhealthModernizationStack',
+  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+    description:
+      'Network infrastructure for the TechHealth patient portal modernization project',
+  }
+);
--- lib/techhealth-modernization-stack.ts
+++ lib/techhealth-modernization-stack.ts
@@ -1,16 +1,113 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class TechhealthModernizationStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  // Export the VPC so EC2 and RDS resources can use it later.
+  public readonly vpc: ec2.Vpc;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /*
+     * TechHealth network
+     *
+     * Creates:
+     * - One VPC
+     * - Two Availability Zones
+     * - One public subnet in each AZ
+     * - One isolated private subnet in each AZ
+     * - No NAT Gateways
+     */
+    this.vpc = new ec2.Vpc(this, 'TechHealthVpc', {
+      vpcName: 'techhealth-patient-portal-vpc',
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TechhealthModernizationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      // VPC address range
+      ipAddresses: ec2.IpAddresses.cidr('10.20.0.0/16'),
+
+      // Use exactly two Availability Zones
+      maxAzs: 2,
+
+      // Do not create NAT Gateways
+      natGateways: 0,
+
+      subnetConfiguration: [
+        {
+          // CDK creates one public subnet in each AZ
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+
+          // EC2 instances launched here can receive public IPs
+          mapPublicIpOnLaunch: true,
+        },
+        {
+          // CDK creates one isolated subnet in each AZ
+          name: 'Database',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+    });
+
+    /*
+     * Tags help identify and manage the resources.
+     */
+    cdk.Tags.of(this.vpc).add(
+      'Project',
+      'TechHealth-Patient-Portal'
+    );
+
+    cdk.Tags.of(this.vpc).add(
+      'Environment',
+      'Development'
+    );
+
+    cdk.Tags.of(this.vpc).add(
+      'ManagedBy',
+      'AWS-CDK'
+    );
+
+    /*
+     * CloudFormation outputs
+     *
+     * These values will appear after cdk deploy.
+     */
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: this.vpc.vpcId,
+      description: 'TechHealth VPC ID',
+    });
+
+    new cdk.CfnOutput(this, 'PublicSubnetIds', {
+      value: this.vpc.publicSubnets
+        .map((subnet) => subnet.subnetId)
+        .join(','),
+      description: 'Public subnet IDs',
+    });
+
+    new cdk.CfnOutput(this, 'PrivateDatabaseSubnetIds', {
+      value: this.vpc.isolatedSubnets
+        .map((subnet) => subnet.subnetId)
+        .join(','),
+      description: 'Isolated private database subnet IDs',
+    });
+
+    new cdk.CfnOutput(this, 'PublicRouteTableIds', {
+      value: this.vpc.publicSubnets
+        .map((subnet) => subnet.routeTable.routeTableId)
+        .join(','),
+      description: 'Public subnet route table IDs',
+    });
+
+    new cdk.CfnOutput(this, 'PrivateRouteTableIds', {
+      value: this.vpc.isolatedSubnets
+        .map((subnet) => subnet.routeTable.routeTableId)
+        .join(','),
+      description: 'Isolated database subnet route table IDs',
+    });
   }
 }
