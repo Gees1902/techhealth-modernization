b3cfe5b31cc8ac1fb105144e192b8458692b7871
Gloria <cchira@outlook.com>
2026-08-23 20:23:05 -0500

    Initial commit

---
+++ test/techhealth-modernization.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechhealthModernization from '../lib/techhealth-modernization-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/techhealth-modernization-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechhealthModernization.TechhealthModernizationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
